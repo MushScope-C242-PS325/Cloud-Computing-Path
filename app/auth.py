6bccb37f4a390598aaa81ba4d6dc6a96c0abd501
Iqbal Al Himni <161192133+iqballhimni@users.noreply.github.com>
2024-12-11 17:35:38 +0700

    fix auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,18 +1,27 @@
-from fastapi import APIRouter, HTTPException, status
-from schemas import UserRegister, UserLogin, ErrorResponse, LoginSuccessResponse
-from auth_service import AuthService
+from fastapi import APIRouter, status, File, Form, UploadFile, Header, HTTPException, Depends
+from typing import Optional
+from .schemas import UserRegister, UserLogin, ErrorResponse, LoginSuccessResponse, ProfileResponse
+from .auth_service import AuthService
 
 router = APIRouter()
 
-@router.post("/register", 
-    response_model=ErrorResponse,
+@router.post("/register",
+    response_model=LoginSuccessResponse,
     status_code=status.HTTP_201_CREATED,
     responses={
         400: {"model": ErrorResponse},
         500: {"model": ErrorResponse}
     })
-async def register(user: UserRegister):
-    return await AuthService.register_user(user)
+
+async def register(
+    name: str,
+    email: str,
+    password: str,
+    photo: Optional[UploadFile] = File(None)
+):
+    auth_service = AuthService()
+    user = UserRegister(name=name, email=email, password=password)
+    return await auth_service.register_user(user, photo)
 
 @router.post("/login",
     response_model=LoginSuccessResponse,
@@ -21,5 +30,33 @@ async def register(user: UserRegister):
         404: {"model": ErrorResponse},
         500: {"model": ErrorResponse}
     })
+
 async def login(user: UserLogin):
-    return await AuthService.login_user(user)
+    auth_service = AuthService()
+    return await auth_service.login_user(user)
+
+
+@router.get("/userProfile",
+    response_model=ProfileResponse,
+    status_code=status.HTTP_200_OK,
+    responses={
+        401: {"model": ErrorResponse},
+        500: {"model": ErrorResponse}
+    }
+)
+
+async def get_profile(authorization: Optional[str] = Header(None)):
+    auth_service = AuthService()
+    if not authorization:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=ErrorResponse(
+                error=True,
+                message="Authorization header is missing"
+            ).dict()
+        )
+    
+    token = await auth_service.extract_token(authorization)
+    user_profile = await auth_service.get_user_profile(token)
+    
+    return user_profile
